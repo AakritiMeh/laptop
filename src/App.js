dcd81b154567c68f020655f8f09cdd407bce253d
Aakriti mehrotra <aakriti.meh.2004@gmail.com>
2023-11-24 21:53:02 +0530

    change

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import "./App.css";
+import Home from "./Home";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
